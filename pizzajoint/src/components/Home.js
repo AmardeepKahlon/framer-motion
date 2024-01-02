cc12354d42e8b680a87733b04b28f1a4a1878767
Amardeep Kahlon <asingh@innow8apps.com>
2024-01-02 15:48:26 +0530

    lesson 8 changes

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div className="home container" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1.5 }}>
+      <motion.h2 animate={{ fontSize: 50, color: '#ff2999' }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button /*animate={{ scale: 1.35 }}*/ whileHover={{ scale: 1.1, textShadow: "0px 0px 8px rgb(255, 255, 255)", boxShadow: "0px 0px 8px rgb(255, 255, 255)"}}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 
--- pizzajoint/src/components/Toppings.js
+++ pizzajoint/src/components/Toppings.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5
+    }
+  }
+}
 
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
+  const [ toppingExists, setToppingExists ] = useState(false);
 
   return (
-    <div className="toppings container">
+    <motion.div className="toppings container" variants={containerVariants} initial='hidden' animate='visible'>
       
       <h3>Step 2: Choose Toppings</h3>
       <ul>
         {toppings.map(topping => {
           let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
+          const handleClick = () => {
+            addTopping(topping);
+            setToppingExists(true);
+          }
+          
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
+            <motion.li key={topping} onClick={handleClick} whileHover={{ scale: 1.3, originX: 0, color: '#f8e111' }} transition={{ type: 'spring', stiffness: 300 }}>
               <span className={spanClass}>{ topping }</span>
-            </li>
+            </motion.li>
           )
         })}
       </ul>
 
-      <Link to="/order">
-        <button>
-          Order
-        </button>
-      </Link>
+      { toppingExists === true && 
+        <Link to="/order">
+          <motion.button initial={{ x: '-100vw'}} animate={{ x: 0 }} transition={{ type: 'spring', stiffness: 180 }} whileHover={{ scale: 1.1, textShadow: "0px 0px 8px rgb(255, 255, 255)", boxShadow: "0px 0px 8px rgb(255, 255, 255)"}}>
+            Order
+          </motion.button>
+        </Link>
+      }
 
-    </div>
+    </motion.div>
   )
 }
 
